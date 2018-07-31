ec80fe5d62ad036fb39092ec6c185cbecd397e2b
AhmedZlazel <ahmedzlazel@AhmedZlazels-Mac.local>
2018-07-31 15:17:48 +0200

    uploading files...

--- testCassini/ViewController.swift
+++ testCassini/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    var imageView = UIImageView()
+    var image: UIImage?{
+        get {
+            return imageView.image
+        }
+        set{
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+
+        }
+    }
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet{
+            scrollView.minimumZoomScale = 0.04
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageURL: URL? {
+        didSet{
+            image = nil
+     
+            if view.window != nil{
+            fetchImage()
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if image == nil{
+            fetchImage()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if imageURL == nil {
+            imageURL = Bundle.main.url(forResource: "Oval", withExtension: "jpg")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func fetchImage(){
+        if imageURL != nil {
+            if let imageData = try? Data(contentsOf: imageURL!){
+                image = UIImage(data: imageData)
+                
+            }
+        }
     }
-
 
 }
 
